6cb5da92e7263940f73f11a400f505dab603f666
JoaquinD26 <98410156+JoaquinD26@users.noreply.github.com>
2024-06-04 20:46:30 +0200

    Co-authored-by: zZDANTI <zZDANTI@users.noreply.github.com>

--- lib/pages/products/popular_iems_widget.dart
+++ lib/pages/products/popular_iems_widget.dart
@@ -12,6 +12,7 @@ class PopularItemsWidget extends StatefulWidget {
 }
 
 class _PopularItemsWidgetState extends State<PopularItemsWidget> {
+
   List<Product> products = [];
 
 
@@ -30,110 +31,111 @@ class _PopularItemsWidgetState extends State<PopularItemsWidget> {
         products = loadedProducts;
       });
 
-
     } else {
       throw Exception('Failed to load products');
     }
 
+  }
 
-    
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+     fetchProducts();
   }
 
   @override
   Widget build(BuildContext context) {
-    fetchProducts();  
 
-    return ListView.builder(
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
-          child: Row(
-            children: products.map((product) {
-              return Padding(
-                padding: EdgeInsets.symmetric(horizontal: 7),
-                child: Container(
-                  width: 200,
-                  height: 250,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 3,
-                        blurRadius: 10,
-                        offset: Offset(0, 3),
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+        child: Row(
+          children: products.map((product) {
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 7),
+              child: Container(
+                width: 200,
+                height: 250,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 3,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ItemPage(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: Image.asset(
+                            product.imageUrl,
+                            height: 130,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 8), // Add some space between image and text
+                      Text(
+                        product.name,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        product.description,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 12),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "${product.price.toString()}€",
+                            style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(
+                            Icons.favorite_border,
+                            color: Colors.red,
+                            size: 26,
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ItemPage(),
-                              ),
-                            );
-                          },
-                          child: Container(
-                            alignment: Alignment.center,
-                            child: Image.asset(
-                              product.imageUrl,
-                              height: 130,
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: 8), // Add some space between image and text
-                        Text(
-                          product.name,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          product.description,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 15,
-                            // fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 12),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "${product.price.toString()}€",
-                              style: TextStyle(
-                                fontSize: 17,
-                                color: Colors.red,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Icon(
-                              Icons.favorite_border,
-                              color: Colors.red,
-                              size: 26,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
                 ),
-              );
-            }).toList(),
-          ),
+              ),
+            );
+          }).toList(),
         ),
       ),
     );
