7967dbb4875fd2e1e10a6c4ffdf9ff95792760ee
JoaquinD26 <98410156+JoaquinD26@users.noreply.github.com>
2024-06-15 21:18:27 +0200

    respuestas...

--- lib/components/posts_from_forum.dart
+++ lib/components/posts_from_forum.dart
@@ -224,6 +224,7 @@ class PostDetailsPageState extends State<PostDetailsPage> {
               ),
             ),
           ),
+          Center(child: Text("Respuestas...")),
           Expanded(
             child: ListView.builder(
               itemCount: postsList.length,
